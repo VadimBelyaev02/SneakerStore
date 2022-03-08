4a902a3315d03f147108fb51c6c179a16a7283e2
= <=>
2022-03-08 13:45:59 +0300

    Added openapi annotations

--- src/main/java/com/vadim/sneakerstore/dto/ProductDto.java
+++ src/main/java/com/vadim/sneakerstore/dto/ProductDto.java
@@ -4,6 +4,7 @@ import com.vadim.sneakerstore.entity.Comment;
 import com.vadim.sneakerstore.entity.Customer;
 import com.vadim.sneakerstore.entity.Photo;
 import com.vadim.sneakerstore.entity.Size;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+@Schema(name = "Product", description = "Contains product's info")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,45 +27,59 @@ public class ProductDto {
 
     private UUID id;
 
+    @Schema(description = "Product's costs")
     @NotNull
     private BigDecimal price;
 
+    @Schema(description = "Product's name")
     @NotBlank
     private String name;
 
+    @Schema(description = "Product's brand")
     @NotBlank
     private String brand;
 
+    @Schema(description = "For what gender")
     @NotBlank
     private String sex;
 
+    @Schema(description = "What a product is used for")
     @NotBlank
     private String destiny; // :D
 
+    @Schema(description = "Product's color")
     @NotBlank
     private String color;
 
+    @Schema(description = "For what time of year is used")
     @NotBlank
     private String season;
 
+    @Schema(description = "Where was made")
     @NotBlank
     private String originCountry;
 
+    @Schema(description = "Product's description")
     @NotBlank
     private String description;
 
+    @Schema(description = "What it's made of")
     @NotBlank
     private String material;
 
+    @Schema(description = "List of size ids")
     private List<UUID> sizeIds;
 
+    @Schema(description = "Main photo of product")
     @NotBlank
     private String preview;
 
+    @Schema(description = "List of photo ids")
     private List<UUID> photoIds;
 
+    @Schema(description = "List of who liked it")
     private List<UUID> customerIds;
 
+    @Schema(description = "List of product's comments")
     private List<UUID> commentIds;
-
 }
