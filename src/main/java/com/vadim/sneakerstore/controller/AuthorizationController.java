51e0c64186e389763dee50d19cec937afe3776e8
= <=>
2022-03-23 00:03:53 +0300

    Coding

--- src/main/java/com/vadim/sneakerstore/controller/AuthorizationController.java
+++ src/main/java/com/vadim/sneakerstore/controller/AuthorizationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 //, allowedHeaders = {"Authorization", "Access-Control-Allow-Origin"}
-//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class AuthorizationController {
