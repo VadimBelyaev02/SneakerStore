1e6d3749e108ba75e06f6969aa1edda9d27b755f
= <=>
2022-03-27 14:12:20 +0300

    Coding

--- src/main/java/com/vadim/sneakerstore/controller/AddressController.java
+++ src/main/java/com/vadim/sneakerstore/controller/AddressController.java
@@ -2,6 +2,8 @@ package com.vadim.sneakerstore.controller;
 
 import com.vadim.sneakerstore.dto.AddressDto;
 import com.vadim.sneakerstore.service.AddressService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,8 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Address Controller", description = "Address CRUD operations")
 @RestController
 @RequestMapping("/api/addresses")
 public class AddressController {
@@ -19,30 +23,35 @@ public class AddressController {
         this.service = service;
     }
 
+    @Operation(description = "Get address by its id")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public AddressDto getAddress(@PathVariable("id") UUID id) {
         return service.getById(id);
     }
 
+    @Operation(description = "Get all addresses")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<AddressDto> getAllAddresses() {
         return service.getAll();
     }
 
+    @Operation(description = "Save a new address")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public AddressDto postAddresses(@Valid @RequestBody AddressDto addressDto) {
         return service.save(addressDto);
     }
 
+    @Operation(description = "Update existed address")
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
     public AddressDto putAddresses(@Valid @RequestBody AddressDto addressDto) {
         return service.update(addressDto);
     }
 
+    @Operation(description = "Delete existed address by its id")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAddress(@PathVariable("id") UUID id) {
